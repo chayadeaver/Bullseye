d5c4679147b58dedbe1e549f3a781ca508e40145
Deaver, Chaya <chaya.deaver@constantcontact.com>
2022-12-08 14:05:08 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Chaya Deaver on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
