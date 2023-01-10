1bc1f3a10e20b414fb4b6079b0d19bf0f49cbda5
Deaver, Chaya <chaya.deaver@constantcontact.com>
2023-01-10 15:43:05 -0500

    adding popup functionality to hit me button

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -8,19 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	
+	@State private var alertIsVisible: Bool = false
+	
+	var body: some View {
+		VStack {
+			Text("🎯🎯🎯\nPUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+				.bold()
+				.kerning(2.0)
+				.multilineTextAlignment(.center)
+				.lineSpacing(4.0)
+				.font(.footnote)
+			Text("89")
+				.kerning(-1.0)
+				.font(.largeTitle)
+				.fontWeight(.black)
+			HStack {
+				Text("1")
+					.bold()
+				Slider(value: .constant(50), in: 1.0...100.0)
+				Text("100")
+					.bold()
+			}
+			Button(action: {
+				print("Hello, SwiftUI")
+				self.alertIsVisible = true
+			}) {
+				Text("Hit Me")
+			}
+			.alert("Hello there!", isPresented: $alertIsVisible){
+				Button("Awesome") {}
+			} message: {
+				Text("This is my first pop-up")
+			}
+		}
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+		ContentView()
+			.previewLayout(.fixed(width: 568, height: 320))
+	}
 }
