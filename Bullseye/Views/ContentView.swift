90a8daea067fa2f75f07b248fbfee369f5e7b591
Deaver, Chaya <chaya.deaver@constantcontact.com>
2023-01-24 14:07:26 -0500

    add slider functionality and tests

--- Bullseye/Views/ContentView.swift
+++ Bullseye/Views/ContentView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
 	
-	@State private var alertIsVisible: Bool = false
+	@State private var alertIsVisible = false
+	@State private var sliderValue = 50.0
+	@State private var game = Game()
 	
 	var body: some View {
 		VStack {
@@ -19,27 +21,28 @@ struct ContentView: View {
 				.multilineTextAlignment(.center)
 				.lineSpacing(4.0)
 				.font(.footnote)
-			Text("89")
+			Text(String(game.target))
 				.kerning(-1.0)
 				.font(.largeTitle)
 				.fontWeight(.black)
 			HStack {
 				Text("1")
 					.bold()
-				Slider(value: .constant(50), in: 1.0...100.0)
+				Slider(value: $sliderValue, in: 1.0...100.0)
 				Text("100")
 					.bold()
 			}
 			Button(action: {
 				print("Hello, SwiftUI")
-				self.alertIsVisible = true
+				alertIsVisible = true
 			}) {
 				Text("Hit Me")
 			}
 			.alert("Hello there!", isPresented: $alertIsVisible){
 				Button("Awesome") {}
 			} message: {
-				Text("This is my first pop-up")
+				let roundedValue = Int(sliderValue.rounded())
+				Text("The slider's value is \(roundedValue).\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round.")
 			}
 		}
 	}
